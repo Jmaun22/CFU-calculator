4b5fdcf23f98a0090fdc1a4dece20d70f292535d
Jesse Maun <jesse@Jesses-MacBook-Air.local>
2022-08-07 17:07:14 -0400

    chnaged colors of the input page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,218 +14,217 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
-
-
-
-
   double colonies = 0;
   double tdf = 0;
   double culture_plated = 0;
+
+  final barColors = red;
+
+  final boxBackgroundColor = yellowOrange;
+
+  final textIconColors = red;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('CFU CALCULATOR'),
+        title: Text('CFU CALCULATOR', style: kCalculateBottomButtonStyle, ),
+        backgroundColor: barColors,
       ),
       // make scrollable to fix overflow
-      body: ListView(
-        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-        children: <Widget>[
-          // space above
+      body: Container(
+        color: orange,
+        child: ListView(
+          keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+          children: <Widget>[
+            // space above
 
-          SizedBox(height: 120),
-          // top row with Number of colines and total dilution factor
-          Row(
-            children: <Widget>[
-              // Number of colinies input box
-              Expanded(
-                child: Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Icon(
-                        Icons.bug_report_outlined,
-                        size: 70.0,
-                      ),
+            SizedBox(height: 120),
+            // top row with Number of colines and total dilution factor
+            Row(
+              children: <Widget>[
+                // Number of colinies input box
+                Expanded(
+                  child: Container(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.bug_report_outlined,
+                          color: textIconColors,
+                          size: 70.0,
+                        ),
 
-                      // number
-                      Text(
-                         colonies.toString(),
-                        style: inputCardStyle,
-                      ),
+                        // number
+                        Text(
+                          colonies.toString(),
+                          style: inputCardStyle,
+                        ),
 
-                      //  input for number of colonies
-                      TextField(
+                        //  input for number of colonies
+                        TextField(
                           // controller:  myController ,
-                        onChanged: (text) {
-                          setState(() {
-                                 colonies = double.parse(text);
+                          onChanged: (text) {
+                            setState(() {
+                              colonies = double.parse(text);
+                            });
+                          },
+
+                          keyboardType: TextInputType.number,
+                          textAlign: TextAlign.center,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: '# of Colonies',
                             
-                          });
-                 
-                        },
-                    
-                        keyboardType: TextInputType.number,
-                        textAlign: TextAlign.center,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: 'Colonies',
+                          ),
                         ),
-                      ),
 
-                      FittedBox(
-                        fit: BoxFit.fitWidth,
-                        child: Text('NUMBER OF COLONIES'),
-                      ),
-                      Text('NOC Result'),
-                    ],
-                  ),
-                  margin: EdgeInsets.all(12.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8.0),
-                    color: Colors.lightGreenAccent,
-                  ),
-                ),
-              ),
-
-              // multiplation icon
-              Icon(
-                Icons.close,
-                color: Colors.green,
-                size: 30.0,
-              ),
-
-              // total dilution factor box
-
-              Expanded(
-                child: Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Icon(
-                        Icons.science_outlined,
-                        size: 70.0,
-                      ),
-
-                      // number
-                      Text(
-                        tdf.toString(),
-                        style: inputCardStyle,
-                      ),
-
-                      //  input diltuion factor
-                      TextField(
-                         onChanged: (text) {
-                          setState(() {
-                                 tdf = double.parse(text);
-                            
-                          });
-                   
-                          print('First text field: $colonies');
-                        },
-                        keyboardType:
-                            TextInputType.numberWithOptions(decimal: true),
-                        textAlign: TextAlign.center,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          hintText: 'TDF',
-                        ),
-                      ),
-                      FittedBox(
-                          fit: BoxFit.fitWidth,
-                          child: Text('TOTAL DILUTION FACTOR')),
-                      // total dilution factor result
-                      Text('TDF Result'),
-                    ],
-                  ),
-                  margin: EdgeInsets.all(12.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8.0),
-                    color: Colors.lightGreenAccent,
-                  ),
-                ),
-              ),
-
-              // Expanded(child: ),
-            ],
-          ),
-
-          SizedBox(
-            height: 5,
-          ),
-
-          // add line spacer inbetween row so it looks like a division bar
-          const Divider(
-            height: 20,
-            thickness: 5,
-            indent: 20,
-            endIndent: 20,
-            color: Colors.black,
-          ),
-          // Spacer
-
-          SizedBox(
-            height: 5,
-          ),
-
-          // middle row with volume of culture plate in ml
-
-          Expanded(
-            child: Container(
-              child: Column(
-                children: <Widget>[
-                  // cultured plate icon
-                  Icon(
-                    Icons.scatter_plot_outlined,
-                    size: 70.0,
-                  ),
-                  Text(
-                    culture_plated.toString(),
-                    style: inputCardStyle,
-                  ),
-
-                  // input
-                  TextField(
-                     onChanged: (text) {
-                          setState(() {
-                               culture_plated = double.parse(text);
-                            
-                          });
-                   
-                          print('First text field: $colonies');
-                        },
-                    keyboardType:
-                        TextInputType.numberWithOptions(decimal: true),
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Culture Plated',
+                        
+                      ],
+                    ),
+                    margin: EdgeInsets.all(12.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      color: boxBackgroundColor,
                     ),
                   ),
-                  Text('CULTURE PLATED mL'),
-                  Text('CULTURE PLATED RESULTS')
-                ],
-              ),
-              margin: EdgeInsets.all(12.0),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8.0),
-                color: Colors.lightGreenAccent,
-              ),
-              height: 200.0,
+                ),
+
+                // multiplation icon
+                Icon(
+                  Icons.close,
+                  color:  textIconColors,
+                  size: 30.0,
+                ),
+
+                // total dilution factor box
+
+                Expanded(
+                  child: Container(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(
+                          Icons.science_outlined,
+                          color: textIconColors,
+                          size: 70.0,
+                        ),
+
+                        // number
+                        Text(
+                          tdf.toString(),
+                          style: inputCardStyle,
+                        ),
+
+                        //  input diltuion factor
+                        TextField(
+                          onChanged: (text) {
+                            setState(() {
+                              tdf = double.parse(text);
+                            });
+
+                            print('First text field: $colonies');
+                          },
+                          keyboardType:
+                              TextInputType.numberWithOptions(decimal: true),
+                          textAlign: TextAlign.center,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Dilution Factor',
+                          ),
+                        ),
+              
+                        // total dilution factor result
+                    
+                      ],
+                    ),
+                    margin: EdgeInsets.all(12.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      color: boxBackgroundColor,
+                    ),
+                  ),
+                ),
+
+                // Expanded(child: ),
+              ],
             ),
-          ),
-          // space below
 
-          SizedBox(
-            height: spacerSpace,
-          ),
+            SizedBox(
+              height: 5,
+            ),
 
-          // bottom caluclate button
-          //  make bottom calculate button
-        ],
+            // add line spacer inbetween row so it looks like a division bar
+            const Divider(
+              height: 20,
+              thickness: 5,
+              indent: 20,
+              endIndent: 20,
+              color: Color(0xFFF37878),
+            ),
+            // Spacer
+
+            SizedBox(
+              height: 5,
+            ),
+
+            // middle row with volume of culture plate in ml
+
+            Expanded(
+              child: Container(
+                child: Column(
+                  children: <Widget>[
+                    // cultured plate icon
+                    Icon(
+                      Icons.scatter_plot_outlined,
+                      size: 70.0,
+                      color: textIconColors,
+                    ),
+                    Text(
+                      culture_plated.toString(),
+                      style: inputCardStyle,
+                    ),
+
+                    // input
+                    TextField(
+                      onChanged: (text) {
+                        setState(() {
+                          culture_plated = double.parse(text);
+                        });
+
+                        print('First text field: $colonies');
+                      },
+                      keyboardType:
+                          TextInputType.numberWithOptions(decimal: true),
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Culture Plated (mL)',
+                      ),
+                    ),
+                  
+                 
+                  ],
+                ),
+                margin: EdgeInsets.all(12.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  color: boxBackgroundColor,
+                ),
+                height: 200.0,
+              ),
+            ),
+            // space below
+
+            SizedBox(
+              height: spacerSpace,
+            ),
+
+            // bottom caluclate button
+            //  make bottom calculate button
+          ],
+        ),
       ),
       bottomNavigationBar: GestureDetector(
         onTap: () {
@@ -235,13 +234,13 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Container(
             child: Container(
                 child: Center(
-                  child: Text('CALCULATE'),
+                  child: Text('CALCULATE',style: kCalculateBottomButtonStyle,),
                 ),
-                margin: EdgeInsets.only(top: 10.0),
+                // margin: EdgeInsets.only(top: 10.0),
                 padding: EdgeInsets.only(bottom: 20.0),
                 width: double.infinity,
                 height: 80.0,
-                color: Colors.lightGreen)),
+                color: barColors)),
       ),
     );
   }
--- lib/widgets/constants.dart
+++ lib/widgets/constants.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 
-
 const inputCardStyle = TextStyle(
   fontSize: 30.0,
   fontWeight: FontWeight.w900,
+  color: red,
 );
+
+const kCalculateBottomButtonStyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+  color: yellowOrange,
+);
+
+const red = Color(0xFFF37878);
+const orange = Color(0xFFFAD9A1);
+const yellowOrange = Color(0xFFF9F9C5);
+const yellow = Color(0xFFFFF323);
+
