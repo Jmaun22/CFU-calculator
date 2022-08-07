746f8d8aa20b4452134fcb5d469163053debe99e
Jesse Maun <jesse@Jesses-MacBook-Air.local>
2022-08-07 13:56:19 -0400

    added input and icons

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,6 +4,8 @@ import 'package:flutter/src/widgets/framework.dart';
 
 import 'package:flutter/material.dart';
 
+double spacerSpace = 40;
+
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -11,8 +13,12 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('CFU CALCULATOR'),
       ),
-      body: Column(
+      // make scrollable to fix overflow
+      body: ListView(
         children: <Widget>[
+          // space above
+
+          SizedBox(height: spacerSpace),
           // top row with Number of colines and total dilution factor
           Row(
             children: <Widget>[
@@ -21,8 +27,24 @@ class HomeScreen extends StatelessWidget {
                 child: Container(
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: <Widget>[
-                      Text('NUMBER OF COLONIES'),
+                      Icon(
+                        Icons.bug_report_outlined,
+                        size: 70.0,
+                      ),
+
+                      //  input for number of colonies
+                      TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Colonies',
+                        ),
+                      ),
+
+                      FittedBox(
+                          fit: BoxFit.fitWidth,
+                          child: Text('NUMBER OF COLONIES')),
                       Text('NOC Result'),
                     ],
                   ),
@@ -34,100 +56,124 @@ class HomeScreen extends StatelessWidget {
                 ),
               ),
 
-
-              // multiplation icon 
-                Icon(
-      Icons.close,
-      color: Colors.green,
-      size: 30.0,
-    ),
-
-
-
+              // multiplation icon
+              Icon(
+                Icons.close,
+                color: Colors.green,
+                size: 30.0,
+              ),
 
               // total dilution factor box
 
               Expanded(
-                  child: Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text('TOTAL DILUTION FACTOR'),
-                    // total dilution factor result
-                    Text('TDF Result'),
-                  ],
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.science_outlined,
+                        size: 70.0,
+                      ),
+                      //  input diltuion factor
+                      TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'TDF',
+                        ),
+                      ),
+                      FittedBox(
+                          fit: BoxFit.fitWidth,
+                          child: Text('TOTAL DILUTION FACTOR')),
+                      // total dilution factor result
+                      Text('TDF Result'),
+                    ],
+                  ),
+                  margin: EdgeInsets.all(12.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8.0),
+                    color: Colors.lightGreenAccent,
+                  ),
                 ),
-                margin: EdgeInsets.all(12.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8.0),
-                  color: Colors.lightGreenAccent,
-                ),
-              ),
               ),
 
               // Expanded(child: ),
             ],
           ),
 
+          SizedBox(
+            height: spacerSpace,
+          ),
+
           // add line spacer inbetween row so it looks like a division bar
-   const Divider(
+          const Divider(
             height: 20,
             thickness: 5,
             indent: 20,
-            endIndent: 0,
+            endIndent: 20,
             color: Colors.black,
           ),
-          // Su
+          // Spacer
 
-
+          SizedBox(
+            height: spacerSpace,
+          ),
 
           // middle row with volume of culture plate in ml
 
-           Expanded(  
-            child: Container(  
+          Expanded(
+            child: Container(
               child: Column(
                 children: <Widget>[
-                  Text('CULTURE PLATED mL'), 
+                  // cultured plate icon
+                  Icon(
+                    Icons.scatter_plot_outlined,
+                    size: 70.0,
+                  ),
+
+                  // input
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Culture Plated',
+                    ),
+                  ),
+                  Text('CULTURE PLATED mL'),
                   Text('CULTURE PLATED RESULTS')
                 ],
-                
-
-
               ),
-                 margin: EdgeInsets.all(12.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8.0),
-                  color: Colors.lightGreenAccent,
-                ),
-
+              margin: EdgeInsets.all(12.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8.0),
+                color: Colors.lightGreenAccent,
+              ),
+                        height: 250.0,
             ),
-
           ),
-         
+          // space below
+
+          SizedBox(
+            height: spacerSpace,
+          ),
 
           // bottom caluclate button
           //  make bottom calculate button
-          GestureDetector(
-            onTap: () { 
-              // route to results page 
-
-            },
-            child: Container(
-              child: Container(  
-                child: Center(  
-                  child: Text('CALCUALTE'),
-                ), 
-                margin: EdgeInsets.only(top: 10.0),
-                padding: EdgeInsets.only(bottom: 20.0), 
-                width: double.infinity,  
-                height: 80.0,
-                color: Colors.lightGreen
-              )
-          
-            ),
-          ),
-
         ],
+      ),
+      bottomNavigationBar: GestureDetector(
+        onTap: () {
+          // route to results page
+          print('tap tap');
+        },
+        child: Container(
+            child: Container(
+                child: Center(
+                  child: Text('CALCUALTE'),
+                ),
+                margin: EdgeInsets.only(top: 10.0),
+                padding: EdgeInsets.only(bottom: 20.0),
+                width: double.infinity,
+                height: 80.0,
+                color: Colors.lightGreen)),
       ),
     );
   }
